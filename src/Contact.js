b5096d555d058026f99522a04d80e484f3ef8bdc
McKlobasa <kl.lampret@gmail.com>
2021-01-23 13:20:33 +0100

    contact posodobljen

--- src/Contact.js
+++ src/Contact.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import Montserrat from './fonts/Montserrat-Regular.ttf'
+import MontserratBold from './fonts/Montserrat-Bold.ttf'
 
 
 const Container = styled.div`
-  margin: auto;
   padding: 50px;
+  width: 100vw;
+  height: fit-content;
+  font-family: 'MontserratBold', sans-serif;
+  display: flex;
+  justify-content: center;
 `
-const ContactLine = styled.p`
-  color: var(--text);
-  font-size: 20px;
+const ContactLine = styled.span`
+  flex: 1;
+  color: var(--secondary);
+  font-size: 28px;
   font-weight: bold;
-
+  padding: 30px;
 `
 
 export default function Contact (props) {
